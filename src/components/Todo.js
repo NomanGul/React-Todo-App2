1f66d6eb39931207513b7e719b7c9928f7c57078
Noman Gul <nomangul2001@gmail.com>
2018-09-15 22:21:04 +0500

    Todo.js commited

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Todo = ({ todos, deleteTodo }) => {
+  // console.log(JSON.parse(todos))
+  const getTodos =
+    todos.length > 0 ? (
+      todos.map(todo => {
+        return (
+          <div className="collection-item" key={todo.id}>
+            <span>{todo.content}</span>
+            <span
+              className="secondary-content"
+              style={{ cursor: "pointer" }}
+              onClick={() => deleteTodo(todo.id)}
+            >
+              <i className="material-icons red-text">delete</i>
+            </span>
+          </div>
+        );
+      })
+    ) : (
+      <div className="collection-item center">
+        You've no Todo's left :) Yay!
+      </div>
+    );
+  return <div className="collection">{getTodos}</div>;
+};
+
+export default Todo;
